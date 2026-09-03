e1eaf549915dfae205efa28b1c2b3948d135ee8f
xiongdl <xiongdl@zju.edu.cn>
2026-09-03 19:50:26 +0800

    fix: support standalone VTA fsim Python

--- python/vta/environment.py
+++ python/vta/environment.py
@@ -29,7 +29,7 @@ from . import intrin
 def get_vta_hw_path():
     """Get the VTA HW path."""
     curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-    vta_hw_default = os.path.abspath(os.path.join(curr_path, "../../../3rdparty/vta-hw"))
+    vta_hw_default = os.path.abspath(os.path.join(curr_path, "../.."))
     VTA_HW_PATH = os.getenv("VTA_HW_PATH", vta_hw_default)
     return os.path.abspath(VTA_HW_PATH)
 
--- python/vta/libinfo.py
+++ python/vta/libinfo.py
@@ -59,17 +59,11 @@ def find_libvta(lib_vta, optional=False):
         Enable error check
     """
     curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-    tvm_library_path = os.environ.get("TVM_LIBRARY_PATH", None)
-    if tvm_library_path is None:
-        tvm_library_path = os.path.join(
-            curr_path,
-            os.pardir,
-            os.pardir,
-            os.pardir,
-            "build",
-        )
+    vta_library_path = os.environ.get("VTA_LIBRARY_PATH", None)
+    if vta_library_path is None:
+        vta_library_path = os.path.join(get_vta_hw_path(), "build")
 
-    lib_search = [tvm_library_path, os.path.join(get_vta_hw_path(), "build")]
+    lib_search = [vta_library_path]
     lib_name = _get_lib_name(lib_vta)
     lib_path = [os.path.join(x, lib_name) for x in lib_search]
     lib_found = [x for x in lib_path if os.path.exists(x)]
